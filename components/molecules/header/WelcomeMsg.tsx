059213a3560a1a1fe7631eaf2970269df1d1ff4b
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-12-14 00:16:11 +0400

    fix

--- components/molecules/header/WelcomeMsg.tsx
+++ components/molecules/header/WelcomeMsg.tsx
@@ -1,8 +1,14 @@
+import { useAuthStore } from '@/store/authStore';
+
 const WelcomeMsg = () => {
+  const user = useAuthStore((state) => state.user);
+
+  console.log(user);
+
   return (
     <div className='space-y-2 mb-4'>
       <h2 className='text-2xl lg:text-4xl text-white font-medium'>
-        Welcome Back {'aq useris asxeli'}
+        Welcome Back {user?.username}
       </h2>
       <p className='text-sm lg:text-base text-[#89b6fd]'>
         This is your Financial Overview Report
